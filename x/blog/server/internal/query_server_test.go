a6d4c1b699854bc10ebdc7c501f5641d17a5b311
David Nix <hello@davidnix.io>
2022-06-17 08:02:38 -0600

    Implement query all comments

---
+++ x/blog/server/internal/query_server_test.go
@@ -0,0 +1,107 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/regen-network/bec/x/blog"
+	"github.com/stretchr/testify/require"
+	dbm "github.com/tendermint/tm-db"
+)
+
+type mockIterator struct {
+	MaxItems int
+	idx      int
+
+	DidClose bool
+}
+
+func newIterator() *mockIterator {
+	// Start at -1 because Next() advances idx on each call and Valid is called first.
+	return &mockIterator{idx: -1}
+}
+
+func (m *mockIterator) Domain() (start []byte, end []byte) {
+	panic("implement me")
+}
+
+func (m *mockIterator) Valid() bool {
+	return m.idx < m.MaxItems
+}
+
+func (m *mockIterator) Next() {
+	m.idx++
+}
+
+func (m *mockIterator) Key() (key []byte) {
+	panic("implement me")
+}
+
+func (m *mockIterator) Value() (value []byte) {
+	return []byte(strconv.Itoa(m.idx))
+}
+
+func (m *mockIterator) Error() error {
+	panic("implement me")
+}
+
+func (m *mockIterator) Close() error {
+	m.DidClose = true
+	return nil
+}
+
+type mockUnmarshaller struct {
+}
+
+func (m *mockUnmarshaller) Unmarshal(bz []byte, proto codec.ProtoMarshaler) error {
+	comment := proto.(*blog.Comment)
+	comment.PostSlug = fmt.Sprintf("slug%s", bz)
+	comment.Author = fmt.Sprintf("author%s", bz)
+	comment.Body = fmt.Sprintf("body%s", bz)
+	return nil
+}
+
+func TestFetchAllComments_AllComments(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("happy path", func(t *testing.T) {
+		iter := mockIterator{MaxItems: 3}
+		var cdc mockUnmarshaller
+		fetch := NewFetchAllComments(&cdc, func(ctx context.Context, prefix []byte) dbm.Iterator {
+			require.NotNil(t, ctx)
+			require.Equal(t, "comment", string(prefix))
+			return &iter
+		})
+
+		req := &blog.QueryAllCommentsRequest{PostSlug: "slug1"}
+		resp, err := fetch(ctx, req)
+
+		require.NoError(t, err)
+		require.Len(t, resp.Comments, 1)
+	})
+
+	t.Run("zero state - post exists but no comments", func(t *testing.T) {
+		iter := mockIterator{MaxItems: 3}
+		var cdc mockUnmarshaller
+		fetch := NewFetchAllComments(&cdc, func(ctx context.Context, prefix []byte) dbm.Iterator {
+			return &iter
+		})
+
+		req := &blog.QueryAllCommentsRequest{PostSlug: "won't find me"}
+		resp, err := fetch(ctx, req)
+
+		require.NoError(t, err)
+		require.Empty(t, resp.Comments)
+	})
+
+	t.Run("post does not exist", func(t *testing.T) {
+		t.Skip("TODO - will need to inject Store interface similar to CreateComment")
+	})
+
+	t.Run("error", func(t *testing.T) {
+		t.Skip("TODO - only error possible seems to be unmarshalling")
+	})
+}
--- x/blog/server/internal/server.go
+++ x/blog/server/internal/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	dbm "github.com/tendermint/tm-db"
 )
 
 // storer is a test hook
@@ -18,6 +19,13 @@ type Server struct {
 	cdc          codec.Codec
 	storeKey     sdk.StoreKey
 	storeFactory func(ctx context.Context, prefix []byte) storer // factory
+
+	// An example how to break up grpc interface methods into smaller functions or service objects that have 1 responsibility.
+	// This prevents one object like Server from becoming large with many methods and struct fields.
+	// Instead, we embed the grpc service methods into Server, thus composing the Server from smaller parts.
+	// The compiler guarantees the grpc service methods are implemented. Therefore, we just have to make sure
+	// the constructor below is updated such that embedded types are non-nil.
+	FetchAllComments
 }
 
 func NewServer(cdc codec.Codec, storeKey sdk.StoreKey) Server {
@@ -28,6 +36,13 @@ func NewServer(cdc codec.Codec, storeKey sdk.StoreKey) Server {
 			sdkCtx := sdk.UnwrapSDKContext(ctx)
 			return prefix.NewStore(sdkCtx.KVStore(storeKey), pref)
 		},
+
+		FetchAllComments: NewFetchAllComments(cdc, func(ctx context.Context, prefix []byte) dbm.Iterator {
+			// TODO: The below boilerplate would have better cohesion if close to Query implementation vs. here in the constructor.
+			sdkCtx := sdk.UnwrapSDKContext(ctx)
+			store := sdkCtx.KVStore(storeKey) // Capturing via closure, may want to add storeKey to test coverage.
+			return sdk.KVStorePrefixIterator(store, prefix)
+		}),
 	}
 
 	return s
