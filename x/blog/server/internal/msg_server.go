39a94a60dd2685e14b3b50f019b1c978e47fda73
David Nix <hello@davidnix.io>
2022-06-16 22:50:54 -0600

    Implement CreateComment

--- x/blog/server/internal/msg_server.go
+++ x/blog/server/internal/msg_server.go
@@ -2,10 +2,14 @@ package internal
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"go.uber.org/multierr"
 
 	"github.com/regen-network/bec/x/blog"
 )
@@ -14,7 +18,6 @@ var _ blog.MsgServer = Server{}
 
 func (s Server) CreatePost(goCtx context.Context, request *blog.MsgCreatePost) (*blog.MsgCreatePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
 	store := prefix.NewStore(ctx.KVStore(s.storeKey), blog.KeyPrefix(blog.PostKey))
 
 	key := []byte(request.Slug)
@@ -39,7 +42,45 @@ func (s Server) CreatePost(goCtx context.Context, request *blog.MsgCreatePost) (
 	return &blog.MsgCreatePostResponse{}, nil
 }
 
-func (s Server) CreateComment(ctx context.Context, comment *blog.MsgCreateComment) (*blog.MsgCreateCommentResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (s Server) CreateComment(ctx context.Context, req *blog.MsgCreateComment) (*blog.MsgCreateCommentResponse, error) {
+	if err := s.validateComment(req); err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
+	blogStore := s.storeFactory(ctx, blog.KeyPrefix(blog.PostKey))
+	if !blogStore.Has([]byte(req.PostSlug)) {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "post slug %s", req.PostSlug)
+	}
+
+	store := s.storeFactory(ctx, blog.KeyPrefix(blog.CommentKey))
+
+	key := sha256.Sum256([]byte(req.PostSlug + req.Author + req.Body))
+	post := blog.Comment{
+		PostSlug: req.PostSlug,
+		Author:   req.Author,
+		Body:     req.Body,
+	}
+
+	bz, err := s.cdc.Marshal(&post)
+	if err != nil {
+		return nil, err
+	}
+	store.Set(key[:], bz)
+
+	return &blog.MsgCreateCommentResponse{Hash: hex.EncodeToString(key[:])}, nil
+}
+
+func (s Server) validateComment(comment *blog.MsgCreateComment) error {
+	var err error
+	// TODO - trim strings?
+	if comment.PostSlug == "" {
+		multierr.AppendInto(&err, errors.New("post slug missing"))
+	}
+	if comment.Author == "" {
+		multierr.AppendInto(&err, errors.New("author missing"))
+	}
+	if comment.Body == "" {
+		multierr.AppendInto(&err, errors.New("body missing"))
+	}
+	return err
 }
--- x/blog/server/internal/server.go
+++ x/blog/server/internal/server.go
@@ -1,22 +1,34 @@
 package internal
 
 import (
+	"context"
+
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// storer is a test hook
 type storer interface {
 	Has(key []byte) bool
 	Set(key, value []byte)
 }
 
 type Server struct {
-	cdc      codec.Codec
-	storeKey sdk.StoreKey
+	cdc          codec.Codec
+	storeKey     sdk.StoreKey
+	storeFactory func(ctx context.Context, prefix []byte) storer // factory
 }
 
 func NewServer(cdc codec.Codec, storeKey sdk.StoreKey) Server {
-	s := Server{cdc: cdc, storeKey: storeKey}
+	s := Server{
+		cdc:      cdc,
+		storeKey: storeKey,
+		storeFactory: func(ctx context.Context, pref []byte) storer {
+			sdkCtx := sdk.UnwrapSDKContext(ctx)
+			return prefix.NewStore(sdkCtx.KVStore(storeKey), pref)
+		},
+	}
 
 	return s
 }
